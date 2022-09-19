70e54f483279ee906925cad5aecc79f0b1257ab5
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-19 21:20:44 +0200

    Signup request

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,23 +2,26 @@ import { hashPassword } from '../../../lib/auth';
 import { connectToDatabase } from '../../../lib/db';
 
 export default async function handler(req, res) {
-  const { email, password } = req.body;
+  if (req.method === 'POST') {
+    const { email, password } = req.body;
 
-  if (
-    !email ||
-    !email.includes('@') ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({ message: 'Invalid input' });
-    return;
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({ message: 'Invalid input' });
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+    const db = await connectToDatabase();
+    await db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: 'User created!' });
   }
-
-  const db = connectToDatabase();
-  await db.collection('users').insertOne({
-    email,
-    password: hashPassword(password),
-  });
-
-  res.status(201).json({ message: 'User created!' });
 }
