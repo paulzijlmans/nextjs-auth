cddd67b4526a7bbfc0b282104bfe7f5633c35d70
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-20 10:27:57 +0200

    Protect Auth Page

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import { signIn } from 'next-auth/client';
 
 import classes from './auth-form.module.css';
+import { useRouter } from 'next/router';
 
 async function createUser(email, password) {
   const response = await fetch('/api/auth/signup', {
@@ -24,6 +25,7 @@ function AuthForm() {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -41,7 +43,10 @@ function AuthForm() {
         email: enteredEmail,
         password: enteredPassword,
       });
-      console.log(result);
+
+      if (!result.error) {
+        router.replace('/profile');
+      }
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
